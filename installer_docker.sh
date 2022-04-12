5674fe8803e01497e2118a1f59bc478d1f1202b8
Neel Ranka <59619624+NeelRanka@users.noreply.github.com>
2022-04-13 01:47:06 +0530

    Update installer_docker.sh

--- installer_docker.sh
+++ installer_docker.sh
@@ -83,15 +83,6 @@ git clone https://github.com/NeelRanka/myPatterns.git
 mkdir ~/.gf
 mv ./myPatterns/*.json ~/.gf
 
-
-#wafw00f install
-echo "https://github.com/EnableSecurity/wafw00f.git[+] installing WafW00f"
-git clone https://github.com/EnableSecurity/wafw00f.git
-cd wafw00f
-python3 setup.py install
-cd ..
-
-
 #gau 
 echo "[+] installing gau"
 go install github.com/lc/gau/v2/cmd/gau@latest
@@ -99,6 +90,30 @@ go install github.com/lc/gau/v2/cmd/gau@latest
 
 git clone https://github.com/NeelRanka/AF1_1.git
 echo "Done cloning AF1_1"
+
+#Entering Tools
+cd ./AF1_1/Tools/
+
+#wafw00f install  => sets to path by default
+echo "https://github.com/EnableSecurity/wafw00f.git[+] installing WafW00f"
+git clone https://github.com/EnableSecurity/wafw00f.git
+cd wafw00f
+python3 setup.py install
+cd ..  #back in Tools
+
+#secretFinder install  => needs to be called by directory
+echo "[+] installing SecretFinder"
+git clone https://github.com/m4ll0k/SecretFinder.git
+cd ./SecretFinder/
+pip3 install -r requirements.txt
+cd .. #back in Tools
+
+#installed waybackFilter
+curl https://raw.githubusercontent.com/NeelRanka/dockerScripts/main/waybackFilter.sh > ./waybackFilter.sh
+
+#exiting Tools
+cd ../
+
 
 echo "importing starter script"
 curl https://raw.githubusercontent.com/NeelRanka/dockerScripts/main/start.sh > start.sh
