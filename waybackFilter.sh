f2edc27579f81d312e3ebdfd36ae8cc35e7f6062
Neel Ranka <59619624+NeelRanka@users.noreply.github.com>
2022-04-13 23:34:04 +0530

    Update waybackFilter.sh

--- waybackFilter.sh
+++ waybackFilter.sh
@@ -19,15 +19,15 @@ mkdir -p $basePath$folderName
 for type in $list
 do
         echo $type
-	gf $type $"'"$basePath$ipfilename$"'" | uniq > $"'"$basePath$folderName$type$".txt"$"'"
+	gf $type $basePath$ipfilename | uniq > $basePath$folderName$type$".txt"
 done
 
 
 # store these at a proper location and merge with the original subdomains (anew/uniq)
 echo "[] Grathering subdomains"
-cat $"'"$basePath$ipfilename$"'" | cut -d'/' -f3 | cut -d':' -f1 | sed 's/^\(\|s\):\/\///g' | uniq > $"'"$basePath$folderName$"subDomains.txt"$"'"
+cat $basePath$ipfilename | cut -d'/' -f3 | cut -d':' -f1 | sed 's/^\(\|s\):\/\///g' | uniq > $basePath$folderName$"subDomains.txt"
 
 echo
 echo "[] Grathering paths for directory brute-force"
 
-cat $"'"$basePath$ipfilename$"'" | rev | cut -d '/' -f 1 | rev | sed 's/^\(\|s\):\/\///g' | sed '/=\|.js\|.gif\|.css\|.jpeg\|.png\|:\|%/d' | uniq > $"'"$basePath$folderName$"bruteforce_list.txt"$"'"
+cat $basePath$ipfilename | rev | cut -d '/' -f 1 | rev | sed 's/^\(\|s\):\/\///g' | sed '/=\|.js\|.gif\|.css\|.jpeg\|.png\|:\|%/d' | uniq > $basePath$folderName$"bruteforce_list.txt"
